1384db6603bf217102a90cabfea8b12e470e4139
varshapagare <varshapare06@gmail.com>
2024-07-19 15:16:01 +0530

    added first-3 files

---
+++ tictactoe.js
@@ -0,0 +1,89 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newgamebtn = document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg= document.querySelector("#msg");
+
+let turnO = true;//playerx,player0
+
+const winPattens =[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame =()=>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box)=>{
+    box.addEventListener("click" ,()=>{
+
+        console.log("box was clicked");
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        }
+        else{
+            box.innerText = "X";
+            turnO = true;
+        }
+    box.disabled = true;
+
+    checkWinner();
+    });
+});
+
+const enableBoxes = ()=>{
+    for(let  box of boxes){
+        box.disabled = false;
+        box.innerHTML ="";
+    }
+};
+
+const disableBoxes= ()=>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+
+const showwinner = (winner)=>{
+   msg.innerHTML = `congratulations,Winner is ${winner}`;
+   msg.Container.classList.remove("hide");
+   disableBoxes();
+}
+
+const checkWinner = () =>{
+    for( let pattern of winPattens){
+        // console.log (pattern [0],pattern [1],pattern [2]);
+        // // console.log ( boxes[pattern [0]],boxes[pattern [1]],boxes[pattern [2]]);
+        // console.log(
+        //     boxes[pattern[0]].innerText,
+        //     boxes[pattern[1]].innerText,
+        //     boxes[pattern[2]].innerText
+        // );
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val !="" && pos2Val != "" && pos3Val !=""){
+            if(pos1Val=== pos2Val && pos2Val===pos3Val){
+                // console.log("winner",pos1Val);//
+                showwinner(pos1Val);
+            }
+        }
+    }
+};
+
+newgamebtn.addEventListener("click",resetGame);
+
+resetbtn.addEventListener("click",resetGame);
